de27a556955793e1e0b0f31f2a799e3489a5b556
Weslley Prado <weslley.prado73@gmail.com>
2022-01-21 15:37:22 -0300

    "Update Readme"

--- CalculateUsingPython/AverageSpeed.py
+++ CalculateUsingPython/AverageSpeed.py
@@ -1,8 +1,18 @@
 '''
-Initial position: X1 = 0
-Speed: 70 km/h
-Distance 1: 8,4 km
-Distance 2: 2 km
-Time 1 = ?
-Time 2 = 30 minutes
+After driving a car on a straight road for eight kilometers and four hundred meters, you stop for lack of gas.
+For the next thirty minutes, you walk another two kilometers along the road until you reach a gas station.
+" a) What was the total displacement from the start of the trip to the gas station?"
+
 '''
+
+print("After driving a car on a straight road for eight kilometers and four hundred meters, you stop for lack of gas.\n"
+      "For the next thirty minutes, you walk another two kilometers along the road until you reach a gas station.")
+
+print("\n a) What was the total displacement from the start of the trip to the gas station?")
+
+distanceDriving = float(input("Write the distance that you drived: "))
+distanceWalking = float(input("Now, write the distance that you walked: "))
+distanceInitial = float(input("Now, write the distance initial: "))
+
+distanceTotal = (distanceDriving + distanceWalking) - distanceInitial
+print(f'The total distance is {distanceTotal}')
