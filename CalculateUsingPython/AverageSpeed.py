a747f8489d43e11c7c841db45aea4b6112065259
Weslley Prado <weslley.prado73@gmail.com>
2022-01-21 15:38:36 -0300

    "Update Readme"

--- CalculateUsingPython/AverageSpeed.py
+++ CalculateUsingPython/AverageSpeed.py
@@ -1,8 +1,7 @@
 '''
 After driving a car on a straight road for eight kilometers and four hundred meters, you stop for lack of gas.
 For the next thirty minutes, you walk another two kilometers along the road until you reach a gas station.
-" a) What was the total displacement from the start of the trip to the gas station?"
-
+a) What was the total displacement from the start of the trip to the gas station?
 '''
 
 print("After driving a car on a straight road for eight kilometers and four hundred meters, you stop for lack of gas.\n"
